8f168105bf4e3d32d7ef8d78cc7c7e23c343a830
rafasumi <rafaelsumitani@dcc.ufmg.br>
2024-04-05 15:51:06 -0300

    Adding attempt_synthesis.tcl

---
+++ scripts/attempt_synthesis.tcl
@@ -0,0 +1,26 @@
+set target_dir "../database/invalid_programs"
+set database_dir "../database"
+
+file mkdir $target_dir
+
+set glob_path [file join $database_dir *.v]
+
+foreach v_file [glob $glob_path] {
+  clear -all
+
+  set exit_code [catch {analyze -sv $v_file}]
+  if { $exit_code != 0 } {
+    puts "analyzed failed with code $exit_code"
+    set file_name [file tail $v_file]
+    file rename -force $v_file [file join $target_dir $file_name]
+    continue
+  }
+
+  set exit_code [catch {elaborate -bbox 1}]
+  if { $exit_code != 0 } {
+    puts "Failed with exit code $exit_code"
+  }
+}
+
+# -batch
+exit
