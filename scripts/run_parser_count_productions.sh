a5d8e4f049810ff884187176c3caa8fdd6d9fd62
rafasumi <rafaelsumitani@dcc.ufmg.br>
2024-05-14 16:17:32 -0300

    Update run_parser_count_productions.sh

--- scripts/run_parser_count_productions.sh
+++ scripts/run_parser_count_productions.sh
@@ -4,47 +4,39 @@
 # as input for the count_productions.py script.
 
 # Check if directory argument is provided
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <directory> <_verible_parser_executable> <output_file>"
+if [ $# -ne 4 ]; then
+    >&2 echo "Usage: $0 <directory> <_verible_parser_executable> <output_file> <n>"
     exit 1
 fi
 
 # Check if the provided argument is a directory
 if [ ! -d "$1" ]; then
-    echo "$1 is not a directory"
+    >&2 echo "$1 is not a directory"
     exit 1
 fi
 
 if [ ! -f "$2" ]; then
-    echo "$2 does not exist or is not a file"
+    >&2 echo "$2 does not exist or is not a file"
     exit 1
 fi
 
 count=1
 total=$(($(ls $1/ -l | wc -l)))
 total=$(($total-1))
-echo "total of programs: $total"
+>&2 echo "total of programs: $total"
 
 mkdir "$1/verible_invalid_programs"
 for file in  "$1"/*.v; do
-
-    echo "Analyzing $count/$total: $file"
-    "$2"  "$file"
-
-    #echo $out
+    >&2 echo "Analyzing $count/$total: $file"
+    "$2" "$file"
 
     if [ $? -ne 1 ]
     then
-        
-        "$2" "--verilog_trace_parser"  "$file" 2>&1 | python3 count_productions.py --output_file $3
-    
+        "$2" "--verilog_trace_parser"  "$file" 2>&1 | python3 count_productions.py "$3" -n "$4"
     else
         mv "$file" "$1/verible_invalid_programs"
-        echo "Error at file: $file"
-        
+        >&2 echo "Error at file: $file"
     fi
-    
-    rm -f aux.txt
+
     ((count++))
 done
-
