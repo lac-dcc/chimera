193c04fa07074e12c8827acd74335df277fc4c67
rafasumi <rafaelsumitani@dcc.ufmg.br>
2024-05-21 10:28:01 -0300

    Update get_num_tokens.py

--- scripts/get_num_tokens.py
+++ scripts/get_num_tokens.py
@@ -1,7 +1,7 @@
 import argparse
 import glob
 import os
-import csv
+import sys
 
 from subprocess import run
 
@@ -13,25 +13,14 @@ def main():
     parser.add_argument('--database_dir', type=str,
                         help='(%(type)s) directory with Verilog programs (default: "%(default)s")',
                         default='../database')
-    parser.add_argument('--output', type=str,
-                        help='(%(type)s) path for the CSV output file (default: "%(default)s")',
-                        default='./results.csv')
     args = parser.parse_args()
 
-    header = ['file', 'num_tokens']
-
-    results = []
-    for file in glob.iglob(os.path.join(args.database_dir, '*.v')):
-        print(f'Running for {file}')
+    print('file,num_tokens', flush=False)
+    for i, file in enumerate(glob.iglob(os.path.join(args.database_dir, '*.v'))):
+        print(f'{i}: Running for {file}', file=sys.stderr)
         proc = run([args.verible_parser_executable,
                    '--printtokens', file], capture_output=True)
-        results.append(
-            {'file': file, 'num_tokens': len(proc.stdout.splitlines()[2:])})
-
-    with open(args.output, 'w') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=header)
-        writer.writeheader()
-        writer.writerows(results)
+        print(f'{file},{len(proc.stdout.splitlines()[2:])}', flush=False)
 
 
 if __name__ == '__main__':
