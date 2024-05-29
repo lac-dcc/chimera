b3c74496aeba1cf581ed63ca6bf2c26fa4994056
rafasumi <rafaelsumitani@dcc.ufmg.br>
2024-05-29 16:50:57 -0300

    Update coverage script

--- scripts/verible_coverage.sh
+++ scripts/verible_coverage.sh
@@ -5,9 +5,9 @@ display_usage() {
 Usage: $0 <_verible_tool_executable> [-h|--help] [-f|--file FILE] [-i|--incremental]
 
 Options:
-  -f, --file FILE        Specifies a file with a list of paths to be used for coverage instead of getting programs from "../database".
-  -i, --incremental      Indicates that coverate should start at the first program and keep previous profilings for the next coverages.
-  -h, --help             Display this help message and exit.\
+  -f, --file FILE     Specifies a file with a list of paths to be used for coverage instead of getting programs from "../database".
+  -i, --incremental   Indicates that coverate should start at the first program and keep previous profilings for the next coverages.
+  -h, --help          Display this help message and exit.\
 "
 }
 
@@ -67,12 +67,12 @@ printf "file,line_coverage,branch_coverage"
 [ $incremental = true ] && printf ",num_files"
 printf "\n"
 
-profraw_files=()
+profdata_files=()
 run_coverage() {
   >&2 echo "$1"
 
   profraw_file="$(basename "$1").profraw"
-  profdata_file="$(basename "$1").profdata"
+  profdata_file="$2.profdata"
 
   if [ $is_obfuscate = true ]; then
     LLVM_PROFILE_FILE=$profraw_file "$verible_exe" < "$1" > /dev/null 2>&1
@@ -80,26 +80,15 @@ run_coverage() {
     LLVM_PROFILE_FILE=$profraw_file "$verible_exe" "$1"
   fi
 
-  if [ $incremental = true ]; then
-    profraw_files+=("$profraw_file")
-    eval llvm-profdata merge -sparse "${profraw_files[@]}" -o "$profdata_file"
+  ((prev = $2 - 1))
+  prev_profdata="$prev.profdata"
+  if [ $incremental = true ] && [ -f "$prev_profdata" ]; then
+    llvm-profdata merge -sparse "$profraw_file" "$prev_profdata" -o "$profdata_file"
   else
     llvm-profdata merge -sparse "$profraw_file" -o "$profdata_file"
   fi
 
-  # Generate report and check which programs have either "0.00%" or "-" in all
-  # coverage columns
-  non_used_files=$(llvm-cov report --ignore-filename-regex="external/*" \
-                    "$verible_exe" -instr-profile="$profdata_file" | \
-                    grep -P '^(?=(.*?(\b0\.00%|-)){4})(?!.*(0\.00%|-){5})' | \
-                    awk '{print $1}')
-
-  ignore_options=$(printf -- '--ignore-filename-regex="%s" ' $non_used_files)
-
-  # Generate the report again, but ignore all programs with 0% coverage in all
-  # columns
-  summary=$(eval llvm-cov report "$ignore_options" \
-                  --ignore-filename-regex="external/*" \
+  summary=$(llvm-cov report --ignore-filename-regex="external/*" \
                   "$verible_exe" -instr-profile="$profdata_file" | \
                   tail -1)
   
@@ -109,31 +98,36 @@ run_coverage() {
   # Output the total line coverage and the total branch coverage
   # If "incremental" is set, also output the number of files
   printf "$1,${line_coverage::-1},${branch_coverage::-1}"
-  [ $incremental = true ] && printf ",${#profraw_files[@]}"
+  [ $incremental = true ] && printf ",$2"
   printf "\n"
 
-  [ $incremental = false ] && rm "$profraw_file"
-  rm "$profdata_file"
+  rm "$profraw_file"
+  if [ $incremental = false ]; then
+    rm "$profdata_file"
+  elif [ -f "$prev_profdata" ]; then
+    rm "$prev_profdata"
+  fi
 }
 
+run_coverage_for_program() {
+  if [ $incremental = false ]; then
+    run_coverage "$1" "$2" &
+  else
+    run_coverage "$1" "$2"
+  fi
+}
+
+i=1
 if [ -z "$file" ]; then
   for input in ../database/*.v; do
-    if [ $incremental = false ]; then
-      run_coverage "$input" &
-    else
-      run_coverage "$input"
-    fi
+    run_coverage_for_program "$input" "$i"
+    ((i++))
   done
-
-  [ $incremental = false ] && wait || rm *.profraw
 else
   while IFS="" read -r input || [ -n "$input" ]; do
-    if [ $incremental = false ]; then
-      run_coverage "$input" &
-    else
-      run_coverage "$input"
-    fi
+    run_coverage_for_program "$input" "$i"
+    ((i++))
   done < $file
-
-  [ $incremental = false ] && wait || rm *.profraw
 fi
+
+[ $incremental = false ] && wait || exit
