82053c4a694ff064a5c249000d00558571696858
Luiza de Melo <ludemelo5452@gmail.com>
2025-01-29 14:29:56 -0300

    Adding elaborate phase information in scripts (#72)
    * modifying scripts to count invalid programs in the elaborate phase
    
    * finished adding elaborate acceptance information
    
    * adding token information from the elaborate phase

--- scripts/attempt_synthesis.tcl
+++ scripts/attempt_synthesis.tcl
@@ -2,11 +2,16 @@ if {![info exists target_dir]} {
   set target_dir "../database/invalid_programs"
 }
 
+if {![info exists target_dir]} {
+  set target_elaborate_dir "../database/invalid_elaborate_programs"
+}
+
 if {![info exists database_dir]} {
   set database_dir "../database"
 }
 
 file mkdir $target_dir
+file mkdir $target_elaborate_dir
 
 set glob_path [file join $database_dir *.v]
 
@@ -23,6 +28,9 @@ foreach v_file [glob $glob_path] {
 
   set exit_code [catch {elaborate -bbox 1}]
   if { $exit_code != 0 } {
-    puts "Failed with exit code $exit_code"
+    puts "elaborate failed with exit code $exit_code"
+    set file_name [file tail $v_file]
+    file rename -force $v_file [file join $target_elaborate_dir $file_name]
+    continue
   }
 }
--- scripts/ngram_experiments.py
+++ scripts/ngram_experiments.py
@@ -28,7 +28,7 @@ def main():
     args = parser.parse_args()
 
     with open(args.results_csv, 'w') as file:
-        print('n_gram,valid_ratio,valid_sum,valid_avg,valid_median,valid_max,valid_min,invalid_avg,invalid_median,invalid_max,invalid_min', file=file)
+        print('n_gram,valid_ratio,valid_sum,valid_avg,valid_median,valid_max,valid_min,invalid_avg,invalid_median,invalid_max,invalid_min, invalid_elaborate_ratio, invalid_elaborate_sum,invalid_elaborate_avg, invalid_elaborate_median, invalid_elaborate_max, invalid_elaborate_min ', file=file)
 
     for n in range(1, 7):
         print(f'---------------{n}GRAM---------------', file=sys.stderr)
@@ -41,15 +41,23 @@ def main():
 
         invalid_dir = os.path.join(base_dir, f'invalid_programs')
         get_num_tokens(invalid_dir, args.verible_parser_executable, csv_file)
+        
+        invalid_elaborate_dir = os.path.join(base_dir, f'invalid_elaborate_programs')
+        get_num_tokens(invalid_elaborate_dir, args.verible_parser_executable, csv_file)
 
         df = pd.read_csv(csv_file)
         df['invalid'] = df.apply(lambda row: row['file'].find('invalid') != -1, axis=1)
+        df['invalid_analyze'] = df.apply(lambda row: row['file'].find('invalid_programs') != -1, axis=1)
+        df['invalid_elaborate'] = df.apply(lambda row: row['file'].find('invalid_elaborate') != -1, axis=1)
 
         valid = df[df['invalid'] == False]['num_tokens']
-        invalid = df[df['invalid'] == True]['num_tokens']
+        invalid_analyze = df[df['invalid_analyze'] == True]['num_tokens']
+        invalid_elaborate = df[df['invalid_elaborate'] == True]['num_tokens']
+        
+        invalid_elaborate_percentage = len(invalid_elaborate) / (len(valid) + len(invalid_elaborate))
 
         with open(args.results_csv, 'a') as file:
-            print(f'{n},{100*(len(valid)/len(df)):.4f},{valid.sum():.2f},{valid.mean():.2f},{valid.median()},{valid.max()},{valid.min()},{invalid.mean():.2f},{invalid.median()},{invalid.max()},{invalid.min()}', file=file)
+            print(f'{n},{100*(len(valid)/len(df)):.4f},{valid.sum():.2f},{valid.mean():.2f},{valid.median()},{valid.max()},{valid.min()},{invalid_analyze.mean():.2f},{invalid_analyze.median()},{invalid_analyze.max()},{invalid_analyze.min()},{invalid_elaborate_percentage:.2f}, {invalid_elaborate.sum():.2f}, {invalid_elaborate.mean():.2f}, {invalid_elaborate.median():.2f}, {invalid_elaborate.max()}, {invalid_elaborate.min()}', file=file)
 
 
 if __name__ == '__main__':
