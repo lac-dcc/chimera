6f6f78e7b57f2a01dbd1464f24a9d90663a91539
RISHIK RAM <44949025+markram1729@users.noreply.github.com>
2025-03-06 18:28:52 +0530

    change : net decl assign or net_variable (#95)
    * change : net decl assign or net_variable
    
    * refactor : variables from global to local

--- src/Chimera.cpp
+++ src/Chimera.cpp
@@ -90,11 +90,32 @@ static std::unique_ptr<Node> getNodeOrFail(const std::string &productionName,
   return it->second(std::move(element));
 }
 
+
+static void isnetdecl(std::vector<std::string> &prods,bool *is_first_netassign,bool *is_first_netvariable) {
+  if (prods[0] == "net_variable") {
+    if (*is_first_netassign == 0) {
+      *is_first_netvariable = true;
+    } else {
+      prods.clear();
+      prods.push_back("net_decl_assign");
+    }
+  } else if (prods[0] == "net_decl_assign") {
+    if (*is_first_netvariable == 0) {
+      *is_first_netassign = true;
+    } else {
+      prods.clear();
+      prods.push_back("net_variable");
+    }
+  }
+}
+
 static std::unique_ptr<Node> buildSyntaxTree(
     const std::unordered_map<std::string, std::unordered_map<std::string, int>>
         &map,
     const int n, std::mt19937 &gen) {
-
+	    
+	bool is_first_netassign = false;
+	bool is_first_netvariable = false;
   auto head = classMap["source_text"]("source_text");
   head->setParent(nullptr);
 
@@ -108,6 +129,7 @@ static std::unique_ptr<Node> buildSyntaxTree(
     std::string context = getNodeContext(curr, n);
     auto prods = chooseProds(map, context, gen);
 
+    isnetdecl(prods,&is_first_netassign,&is_first_netvariable);
     std::vector<std::unique_ptr<Node>> children;
     context = getNodeContext(curr, n - 1);
     if (!context.empty())
