7930b890437d874483b96e7b977470d881086864
joaovam <amorimvictorjoao3@gmail.com>
2025-03-26 12:34:59 -0300

    adding script to generate template code

--- scripts/generate_template.py
+++ scripts/generate_template.py
@@ -1,4 +1,9 @@
-class_names = [#Add set of productions here
+"""
+This script generate the basic code to add more productions to ChiGen.
+
+The visitor files should be customized accordingly to specific needs of each new production.
+""" 
+class_names = [#add name of classes here
     ]
 
 def insert_strings(template: str, placeholders: list, values: list) -> str:
@@ -18,14 +23,61 @@ def insert_strings(template: str, placeholders: list, values: list) -> str:
 
     return template
 
+def generate_enum_entries():
+    """Generates the entries to add to the NodeType enum in AST.h"""
+    with open("enum_entries.txt", "w") as out:
+        for c in class_names:
+            out.write(f"{c.upper()},\n")
+            
 
-# Example usage
-if __name__ == "__main__":
-    template_string = """{"{lower}",
+def generate_AST_classes():
+    """Generates the basic classes for the new nodes, each node must have its own class to be used in the visitors. The output should be placed inside AST.h"""
+    template = """ class {capitalized} : public Node {
+    public:
+    {capitalized}(std::string element);
+    {capitalized}({capitalized} &&) = default;
+    {capitalized} &operator=({capitalized} &&) = default;
+    }; """
+
+    placeholders = ["{capitalized}"]
+
+    with open("ast_classes.txt", "w") as out:
+        for c in class_names:
+            result = insert_strings(template, placeholders, [c.capitalize()])
+            out.write(f"{result}\n")
+
+
+def generate_AST_implementations():
+    """Generates the implementation for the classes created in generate_AST_classes(). The output should be placed in AST.cpp"""
+    template = """{capitalized}::{capitalized}(std::string element) {
+  this->type = NodeType::{upper};
+  this->setElement(element);
+} """
+
+    placeholders = ["{capitalized}", "{upper}"]
+
+    with open("ast_implementations.txt", "w") as out:
+        for c in class_names:
+            result = insert_strings(template, placeholders, [c.capitalize(), c.upper()])
+            out.write(f"{result}\n")
+
+def generate_map_entries():
+    """Generates the entries for the classMap inside AST.cpp"""
+    template = """
+ {"{lower}",
    [](std::string &&f){
       return std::make_unique<{capitalized}>(f);
-   }},"""
+   }}"""
+
     placeholders = ["{lower}", "{capitalized}"]
-    for c in class_names:
-        result = insert_strings(template_string, placeholders, [c.lower(), c])
-        print(result)
+
+    with open("map_entries.txt", "w") as out:
+        for c in class_names:
+            result = insert_strings(template, placeholders, [c.lower(), c.capitalize()])
+            out.write(f",{result}\n")
+
+if __name__ == "__main__":
+    generate_enum_entries()
+    generate_AST_classes()
+    generate_AST_implementations()
+    generate_map_entries()
