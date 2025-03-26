ee83b5ee09c25c84d74d0cdeb3ab92ffeb06d877
joaovam <amorimvictorjoao3@gmail.com>
2025-03-26 13:37:42 -0300

    adding methods to script

--- scripts/generate_template.py
+++ scripts/generate_template.py
@@ -3,8 +3,7 @@ This script generate the basic code to add more productions to ChiGen.
 
 The visitor files should be customized accordingly to specific needs of each new production.
 """ 
-class_names = [#add name of classes here
-    ]
+class_names = []
 
 def insert_strings(template: str, placeholders: list, values: list) -> str:
     """
@@ -76,8 +75,89 @@ def generate_map_entries():
             result = insert_strings(template, placeholders, [c.lower(), c.capitalize()])
             out.write(f",{result}\n")
 
+def generate_type_inference_std_visitor():
+    """Generates the entries for the classMap inside AST.cpp"""
+    template = """
+ constraintSet TypeInferenceVisitor::visit({capitalized} *node, typeId type) {
+  return defaultVisitor(node, type);
+}"""
+
+    placeholders = ["{capitalized}"]
+
+    with open("type_inference.txt", "w") as out:
+        for c in class_names:
+            result = insert_strings(template, placeholders, [c.capitalize()])
+            out.write(f"{result}\n")
+
+def generate_type_inference_std_visitor_decl():
+    """Generates the entries for the classMap inside AST.cpp"""
+    template = """virtual constraintSet visit({capitalized} *node, typeId type) override;"""
+
+    placeholders = ["{capitalized}"]
+
+    with open("type_inference_h.txt", "w") as out:
+        for c in class_names:
+            result = insert_strings(template, placeholders, [c.capitalize()])
+            out.write(f"  {result}\n\n")
+
+def generate_visitor_switch_entries():
+    """Generates the entries for the switch with arguments inside Visitor.h"""
+    template = """case NodeType::{upper}:
+      return visit(dynamic_cast<{capitalized} *>(node));"""
+
+    placeholders = ["{capitalized}", "{upper}"]
+
+    with open("switch_entries_no_arg.txt", "w") as out:
+        for c in class_names:
+            result = insert_strings(template, placeholders, [c.capitalize(), c.upper()])
+            out.write(f"  {result}\n\n")
+
+def generate_visitor_switch_entries_with_arg():
+    """Generates the entries for the switch without arguments inside Visitor.h"""
+    template = """case NodeType::{upper}:
+      return visit(dynamic_cast<{capitalized} *>(node), arg);"""
+
+    placeholders = ["{capitalized}", "{upper}"]
+
+    with open("switch_entries_arg.txt", "w") as out:
+        for c in class_names:
+            result = insert_strings(template, placeholders, [c.capitalize(), c.upper()])
+            out.write(f"  {result}\n\n")
+
+def generate_visitor_default_methods():
+    """Generates the entries for the visitor methods inside Visitor.h"""
+    template = """virtual R visit({capitalized} *node) {
+    for (const std::unique_ptr<Node> &child : node->getChildren()) {
+      applyVisit(child.get());
+    }
+
+    return R();
+  }
+
+  virtual R visit({capitalized} *node, T arg) {
+    for (const std::unique_ptr<Node> &child : node->getChildren()) {
+      applyVisit(child.get(), arg);
+    }
+
+    return R();
+  }"""
+
+    placeholders = ["{capitalized}"]
+
+    with open("visitor_default.txt", "w") as out:
+        for c in class_names:
+            result = insert_strings(template, placeholders, [c.capitalize()])
+            out.write(f"  {result}\n\n")
+
+
+
 if __name__ == "__main__":
     generate_enum_entries()
     generate_AST_classes()
     generate_AST_implementations()
     generate_map_entries()
+    generate_type_inference_std_visitor()
+    generate_type_inference_std_visitor_decl()
+    generate_visitor_switch_entries()
+    generate_visitor_switch_entries_with_arg()
+    generate_visitor_default_methods()
