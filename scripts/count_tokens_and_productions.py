0c445343ae0aef8e99327d53576fb98bae224fb0
luizademelo <ludemelo5452@gmail.com>
2025-03-28 13:48:58 -0300

    refactoring count_tokens_and_productions.py script

--- scripts/count_tokens_and_productions.py
+++ scripts/count_tokens_and_productions.py
@@ -2,19 +2,7 @@ from itertools import count
 import json
 import os
 
-# json_files = [
-#     "1gram.json", "2gram.json", "3gram.json", "4gram.json", "5gram.json", "6gram.json",
-#     "1gram_size_test.json", "2gram_size_test.json", "3gram_size_test.json", "4gram_size_test.json", "5gram_size_test.json", "6gram_size_test.json",
-#     "1gram_test.json", "2gram_test.json", "3gram_test.json", "4gram_test.json", "5gram_test.json", "6gram_test.json",
-#     "1gram_test_only_logic.json", "2gram_test_only_logic.json", "3gram_test_only_logic.json", "4gram_test_only_logic.json", "5gram_test_only_logic.json", "6gram_test_only_logic.json"
-# ]
-
-# json_files = [
-#     "json100.txt", "json2.txt"
-# ]
-
 json_files = [
-    "json_verismith.txt"
     # "json_chigen_1gram_1024.txt", "json_chigen_2gram_1024.txt", "json_chigen_3gram_1024.txt", 
     # "json_chigen_4gram_1024.txt", "json_chigen_5gram_1024.txt", "json_chigen_6gram_1024.txt",
     # "json_chigen_1gram_16000.txt", "json_chigen_2gram_16000.txt", "json_chigen_3gram_16000.txt", 
@@ -25,7 +13,7 @@ json_files = [
     # "json_chigen_4gram_4096.txt", "json_chigen_5gram_4096.txt", "json_chigen_6gram_4096.txt",
     # "json_chigen_1gram_8192.txt", "json_chigen_2gram_8192.txt", "json_chigen_3gram_8192.txt", 
     # "json_chigen_4gram_8192.txt", "json_chigen_5gram_8192.txt", "json_chigen_6gram_8192.txt", "json_chibench.txt"
-    # "json_verismith.txt", "vloghammer.txt", "../json/1gram_size_test.json", "../json/1gram.json", "../json/1gram_test.json"
+    # "json_verismith.txt", "vloghammer.txt", "../json/1gram_size_test.json"
 ]
 
 # directory_path = "~/chimera/*.v"
@@ -41,15 +29,14 @@ tokens_chigen3 = set()
 tokens_chigen4 = set()
 tokens_chigen5 = set()
 tokens_chigen6 = set()
+
 def load_grammar(filename): 
     grammar = {}
-    # filename = 'json/' + filename
     if os.path.isfile(filename): 
         with open(filename, 'r') as file: 
             grammar = json.load(file)
     else: 
         print("file not found")
-    # print(grammar)
     return grammar
 
 def get_productions(grammar): 
@@ -83,14 +70,3 @@ if __name__ == '__main__':
         print("Total productions: " + str(len(productions)))
         print("Total tokens: " + str(len(tokens)))
         print()
-        
-    #     if grammar_file == 'json_chibench.txt': 
-    #         tokens_chibench = tokens 
-    #     else: 
-    #         for t in tokens: 
-    #             tokens_chigen.add(t)
-              
-            
-    # for t in tokens_chibench: 
-    #     if t not in tokens_chigen: 
-    #         print(t)
